56f75d717e16dc63cc03bba365a69bea322dcae1
Mauricio Pereira <mauriciovianense@hotmail.com>
2022-05-09 15:52:40 +0100

    Nada

--- src/main/java/View/View.java
+++ src/main/java/View/View.java
@@ -15,15 +15,22 @@ public class View {
     public static void printer(Object o){System.out.println(o);}
 
     /**Prints questions related to the House */
-    public static void checkHouse(int i) {
+    public static void controlHouse(int i) {
         if(i == 1) System.out.print("Choose your Room: ");
-        else if(i == 2) System.out.print("Choose you Device: ");
-        else if(i == 3) System.out.print("Choose the Volume in the Speaker: ");
-        else  if(i == 4) System.out.print("Choose the Tonality in the Bulb: ");
+        else if(i == 2) System.out.print("Choose a Device in the Room: ");
+        else if(i == 3) System.out.print("Choose the Room you want to remove: ");
+        else if(i == 4) System.out.print("Add a Room: ");
+    }
+
+    public static void controlDevice(int i){
+        if(i == 1) System.out.print("Choose your Device: ");
+        else if(i==2) System.out.print("Choose the Volume in the Speaker: ");
+        else if(i==3) System.out.print("Choose the Tonality in the Bulb: ");
         else if(i == 5) System.out.print("Choose the Camera Resolution ");
-        else if(i == 6) System.out.print("Choose the Room you want to remove: ");
-        else if(i == 7) System.out.print("Choose the Device you want to remove: ");
-        else if(i == 8) System.out.print("Choose the Camera Resolution ");
+        else if(i == 6) System.out.print("Choose the Device you want to remove: ");
+        else if(i == 7) System.out.print("Add a Device: ");
+
+
     }
 
 
