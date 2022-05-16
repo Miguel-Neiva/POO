53e7001df5ece0478abad93186618e7fefe08b45
Maurício Pereira <mauriciovianense@hotmail.com>
2022-05-16 15:59:11 +0100

    tax and priceKW

--- src/main/java/Simulation/Simulation.java
+++ src/main/java/Simulation/Simulation.java
@@ -2,9 +2,16 @@ package Simulation;
 
 import java.time.LocalDateTime;
 import EnergySeller.EnergySeller;
+import House.House;
 
 public class Simulation {
 
+    public enum Free {
+        On,
+        Off
+    }
 
-
+    public double Calc(House house){
+        
+    }
 }
